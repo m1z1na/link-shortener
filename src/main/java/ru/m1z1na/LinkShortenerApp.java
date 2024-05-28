10eed94717e5685aaf1b3808ca9a69f198c52f47
m1z1na <51057800+m1z1na@users.noreply.github.com>
2024-05-28 19:53:21 +0300

    Init

---
+++ src/main/java/ru/m1z1na/LinkShortenerApp.java
@@ -0,0 +1,12 @@
+package ru.m1z1na;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShortenerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkShortenerApp.class);
+    }
+}
